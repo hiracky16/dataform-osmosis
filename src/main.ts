3941ca7d49bd92b609433ec6c2ad44a6ff77a2c5
hiracky16 <h.piiice16@gmail.com>
2024-10-21 09:17:59 +0900

    fix: 🐛 version

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .name("dataform-osmosis")
   .description("CLI tool for Dataform Osmosis")
-  .version("0.1.0");
+  .version("0.1.1");
 
 program
   .command("valid")
